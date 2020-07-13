9bd3ddda0bff26674f24a1628458f55811d9179f
Greg Pina <pinagm2825@gmail.com>
2020-07-13 14:37:16 -0400

    spelling error (whoops!)

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,13 +1,14 @@
 import React from "react"
 import CharacterItem from "./CharacterItem"
+import Spinner from "../UI/header"
 
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
-    <h1> Loading... </h1>
+    <Spinner />
   ) : (
     <section className="cards">
       {items.map((item) => (
-        <CharacterItem key={item.char_id}>item={item} </CharacterItem>
+        <CharacterItem key={item.char_id} item={item}></CharacterItem>
       ))}
     </section>
   )
