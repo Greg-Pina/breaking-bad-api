a1d59c484b85fde39398ca08d5f894e0d000ff10
Greg Pina <pinagm2825@gmail.com>
2021-10-29 16:32:41 -0400

    clearing commits

--- src/components/UI/header.js
+++ src/components/UI/header.js
@@ -1,12 +1,12 @@
-import React from "react"
-import logo from "../../img/logo.png"
+import React from 'react'
+import logo from '../../img/Logo.png'
 
 const header = () => {
-  return (
-    <header className="center">
-      <img src={logo} alt="" />
-    </header>
-  )
+	return (
+		<header className="center">
+			<img src={logo} alt="" />
+		</header>
+	)
 }
 
 export default header
